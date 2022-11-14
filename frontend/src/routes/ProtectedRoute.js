8054d7b1ce8380098f95b944af331369756c79fb
nortadzhiev-rustam <67524074+nortadzhiev-rustam@users.noreply.github.com>
2022-11-14 11:47:09 +0100

    modified:   frontend/src/routes/ProtectedRoute.js

--- frontend/src/routes/ProtectedRoute.js
+++ frontend/src/routes/ProtectedRoute.js
@@ -14,13 +14,8 @@ useEffect(()=> {
   const isAuth = useSelector((state) => state.user.user.isAuth);
   const isLoading = useSelector((state) => state.user.isLoading);
   if (isLoading) return <Spinner loading={isLoading}/>
-<<<<<<< HEAD
   console.log(isLoggedIn);
-  return isLoggedIn ? <Outlet /> : <Navigate to='/login' />;
-=======
-
   return isAuth ? <Outlet /> : <Navigate to='/login' />;
->>>>>>> 513df6e5b4260c9b2d209e61f9af55a313bf1023
 };
 
 export default ProtectedRoute;
